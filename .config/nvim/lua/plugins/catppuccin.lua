5281ae1e9d8143b187bfec4257bac0add1269d7a
Siarhei Ryzhko <yndarven@gmail.com>
2025-02-26 09:48:22 +0300

    disable italics

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,10 +1,19 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+      dim_inactive = { enabled = true, shade = "dark", percentage = 0.15 },
+      no_italic = true,
+      default_integrations = true,
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
     },
   },
-
 }
