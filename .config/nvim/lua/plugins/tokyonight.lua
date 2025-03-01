37c0fcfdea23006aa4dcbf2a4fed00490eee1496
Siarhei Ryzhko <yndarven@gmail.com>
2025-03-01 18:53:53 +0300

    up

---
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    style = "night",
+    transparent = false,
+    styles = {
+      comments = { italic = false },
+      keywords = { italic = false },
+    },
+  },
+}
