fee21de9814dbc9f852687ce3993c501fa0da19d
Siarhei Ryzhko <yndarven@gmail.com>
2025-03-10 17:14:38 +0300

    change theme

--- .config/nvim/lua/plugins/kanagawa.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "rebelot/kanagawa.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {
-    theme = "kanagawa",
-    transparency = true,
-  },
-}
---
+++ .config/nvim/lua/plugins/rosepine.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      extend_background_behind_borders = false,
+      styles = {
+        transparency = true,
+      },
+    },
+  },
+}
