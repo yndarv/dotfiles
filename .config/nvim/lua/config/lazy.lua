223ce2446801a0d2d513ccecdbcfd915044b7161
Siarhei Ryzhko <yndarven@gmail.com>
2025-03-10 17:13:57 +0300

    change theme

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -5,7 +5,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out,                            "WarningMsg" },
+      { out, "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -17,7 +17,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "rose-pine" } },
     -- import/override with your plugins
     { import = "plugins" },
   },
@@ -36,8 +36,8 @@ require("lazy").setup({
   -- install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
     enabled = false, -- check for plugin updates periodically
-    notify = false,  -- notify on update
-  },                 -- automatically check for plugin updates
+    notify = false, -- notify on update
+  }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
