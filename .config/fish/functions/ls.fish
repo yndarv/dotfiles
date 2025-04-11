7f874d41a4677ec0e3afc67a118c413cf2f743cd
Siarhei Ryzhko <yndarven@gmail.com>
2025-04-12 02:01:00 +0300

    update fish

---
+++ .config/fish/functions/ls.fish
@@ -0,0 +1,4 @@
+function ls --wraps='eza --long --icons=auto --header --all' --description 'alias ls eza --long --icons=auto --header --all'
+  eza --long --icons=auto --header --all $argv
+        
+end
